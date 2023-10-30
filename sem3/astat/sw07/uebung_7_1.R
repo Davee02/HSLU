b9dca26fe3f71af267bf6790482d66c9a2fe8039
David <davidhodel6@gmail.com>
2023-10-30 16:14:10 +0100

    add astat sw07

---
+++ sem3/astat/sw07/uebung_7_1.R
@@ -0,0 +1,10 @@
+# a)
+1-pnorm(q=185,mean=180,sd=7.4)
+pnorm(q=180,mean=180,sd=7.4) - pnorm(q=170,mean=180,sd=7.4)
+
+# b)
+qnorm(p=0.25,mean=180,sd=7.4)
+qnorm(p=0.75,mean=180,sd=7.4)
+
+# c)
+qnorm(p=0.95,mean=180,sd=7.4)
---
+++ sem3/astat/sw07/uebung_7_2.R
@@ -0,0 +1,9 @@
+# a)
+pnorm(q=2.5,mean=4,sd=1.25)
+1-pnorm(q=5,mean=4,sd=1.25)
+
+# b)
+pnorm(q=4.5,mean=4,sd=1.25) - pnorm(q=3,mean=4,sd=1.25)
+
+# c)
+qnorm(p=0.98,mean=4,sd=1.25)
