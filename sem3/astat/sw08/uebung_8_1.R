93956107a8e3004a2c256191cf93b02429ae7dda
David <davidhodel6@gmail.com>
2023-11-06 15:31:15 +0100

    add astat sw08

---
+++ sem3/astat/sw08/uebung_8_1.R
@@ -0,0 +1,8 @@
+# a)
+pnorm(q=10,mean=8.2,sd=1)
+
+# b)
+pnorm(q=10,mean=8.2,sd=1) - pnorm(q=5,mean=8.2,sd=1)
+
+# c)
+1 - pnorm(q=20,mean=8.2,sd=1)
---
+++ sem3/astat/sw08/uebung_8_3.R
@@ -0,0 +1,5 @@
+# a)
+pnorm(q=82,mean=77,sd=3) - pnorm(q=72,mean=77,sd=3)
+
+# b)
+pnorm(q=82,mean=77,sd=1.875) - pnorm(q=72,mean=77,sd=1.875)
