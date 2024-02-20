f0482d029f52fc79f76d65bcafd3627ff45cd968
David Hodel <davidhodel6@gmail.com>
2024-02-20 13:28:53 +0100

    add csa sw01

---
+++ sem4/csa/Ueb01_HelloWorldConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        PrintMessage("Hello World", false);
+        PrintMessage("Hello World", true);
+        Console.Read();
+    }
+
+    private static void PrintMessage(string msg, bool upperCase)
+    {
+        if (upperCase)
+        {
+            msg = msg.ToUpper();
+        }
+        Console.WriteLine(msg);
+    }
+}
