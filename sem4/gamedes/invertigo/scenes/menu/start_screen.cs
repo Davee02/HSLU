6006f9f914f8627b18b015736d85135091c65b59
David <davidhodel6@gmail.com>
2024-05-26 10:36:49 +0200

    add narrative to invertigo

--- sem4/gamedes/invertigo/scenes/menu/start_screen.cs
+++ sem4/gamedes/invertigo/scenes/menu/start_screen.cs
@@ -2,18 +2,41 @@ using Godot;
 
 public partial class start_screen : Control
 {
+    private Control _controlsNode;
+    private Control _storyNode;
+    private int _clickCount = 0;
+    private double _timeSinceLastClick = 0;
+
     public override void _Ready()
     {
+        _controlsNode = GetNode<Control>("Controls");
+        _storyNode = GetNode<Control>("Story");
+
+        _storyNode.Visible = false;
         GetTree().Paused = true;
     }
 
     public override void _Process(double delta)
 	{
-		if (Input.IsAnythingPressed())
+        _timeSinceLastClick += delta;
+
+        if (Input.IsAnythingPressed() && _timeSinceLastClick >= 0.2)
         {
-            GetTree().Paused = false;
-            Messanger.Instance.EmitSignal(Messanger.SignalName.GameStarted);
-            Free();
+            _clickCount++;
+            _timeSinceLastClick = 0;
+
+            if (_clickCount == 1)
+            {
+                _storyNode.Visible = true;
+                _controlsNode.Visible = false;
+
+            }
+            else
+            {
+                GetTree().Paused = false;
+                Messanger.Instance.EmitSignal(Messanger.SignalName.GameStarted);
+                Free();
+            }
         }
     }
 }
